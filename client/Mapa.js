819d3d270748182d3825cc91fdb404d335ecb869
jlbali <juan.lucas.bali@gmail.com>
2018-11-27 09:32:02 -0300

    Primeros intentos con Leaflet

--- client/Mapa.js
+++ client/Mapa.js
@@ -9,9 +9,28 @@ class Mapa extends Component {
     return (
       <div className="main­content">
           Aca habria un mapa!!
+          <div id="mapa"></div>
       </div>
     );
   }
+
+  componentDidMount() {
+    // Especificando mapa y su centro. 
+    var myMap = L.map('mapa').setView([51.505, -0.09], 13);
+
+    // Capa Mapbox.
+    var mapboxLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'your.mapbox.access.token'
+    });
+    // Agregándolo al mapa.
+    //mapboxLayer.addTo(myMap);
+
+
+  }
+
 }
 
   
